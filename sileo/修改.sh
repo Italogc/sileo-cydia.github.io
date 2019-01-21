32ef90bfbf898c6a6a6cb86593fb49ba3e145285
他是个疯子 <409774743@qq.com>
2019-01-21 16:25:37 +0800

    .

--- sileo/修改.sh
+++ sileo/修改.sh
@@ -2,16 +2,17 @@
 
 
 #描述
-perl -p -i -e "s/852360/无需签名安装ipa应用/g"  样本2.json
+perl -p -i -e "s/Sileo简体中文包。/g"  样本2.json
 #描述2
 perl -p -i -e "s/🀙/🀙/g"  样本2.json
 #大小
-perl -p -i -e "s/852362/1488/g"  样本2.json
+perl -p -i -e "s/852362/3/g"  样本2.json
 #支持版本
-perl -p -i -e "s/852363/ios10-11/g"  样本2.json
+perl -p -i -e "s/852363/ios11/g"  样本2.json
+#perl -p -i -e "s/852363/ios11/g"  样本2.json
 #更新日期
-perl -p -i -e "s/852364/31.1/g"  样本2.json
+perl -p -i -e "s/852364/2019-01-21/g"  样本2.json
 
-mv 样本2.json appsyncunified.json
+mv 样本2.json sileozh.json
 
 cp 样本.json 样本2.json
