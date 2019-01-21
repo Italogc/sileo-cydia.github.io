f8a3caf23915b54b9d022957c89a1d2e2c185a0b
他是个疯子 <409774743@qq.com>
2019-01-21 15:29:23 +0800

    .

--- sileo/修改.sh
+++ sileo/修改.sh
@@ -2,16 +2,16 @@
 
 
 #描述
-perl -p -i -e "s/852360/来电时自动应答.破解汉化版/g"  样本2.json
+perl -p -i -e "s/852360/无需签名安装ipa应用/g"  样本2.json
 #描述2
 perl -p -i -e "s/🀙/🀙/g"  样本2.json
 #大小
 perl -p -i -e "s/852362/1488/g"  样本2.json
 #支持版本
-perl -p -i -e "s/852363/ios11/g"  样本2.json
+perl -p -i -e "s/852363/ios10-11/g"  样本2.json
 #更新日期
-perl -p -i -e "s/852364/2019-01-21/g"  样本2.json
+perl -p -i -e "s/852364/31.1/g"  样本2.json
 
-mv 样本2.json answeringmachinex.json
+mv 样本2.json appsyncunified.json
 
 cp 样本.json 样本2.json
