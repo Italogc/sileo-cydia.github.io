c3d1a9881067f6cfc60f6fe758fc1d6fb6271c46
他是个疯子 <409774743@qq.com>
2019-01-21 19:04:59 +0800

    .

--- sileo/修改.sh
+++ sileo/修改.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/ 隐藏状态栏指示器/g"  样本2.json
+perl -p -i -e "s/852359/屏蔽越狱检测/g"  样本2.json
 #描述
-perl -p -i -e "s/852360/隐藏状态栏指示器(通话,录制屏幕,定位).暂时隐藏录屏指示器就行,隐藏其他原素,会使录屏有bug/g"  样本2.json
+perl -p -i -e "s/852360/屏蔽越狱监测.开启支付设置/g"  样本2.json
 #大小
-perl -p -i -e "s/852362/196/g"  样本2.json
+perl -p -i -e "s/852362/47/g"  样本2.json
 
-mv 样本2.json silentrecorder.json
+mv 样本2.json libertylite.json
 
 cp 样本.json 样本2.json
 
