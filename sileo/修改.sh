5dfe3e62486c447884923252984c2326573dedfa
他是个疯子 <409774743@qq.com>
2019-01-21 23:07:59 +0800

    .

--- sileo/修改.sh
+++ sileo/修改.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 标题
-perl -p -i -e "s/852359/屏蔽越狱检测/g"  样本2.json
+perl -p -i -e "s/852359//g"  样本2.json
 #描述
-perl -p -i -e "s/852360/屏蔽越狱监测.开启支付设置/g"  样本2.json
+perl -p -i -e "s/852360//g"  样本2.json
 #大小
-perl -p -i -e "s/852362/47/g"  样本2.json
+perl -p -i -e "s/852362//g"  样本2.json
 
-mv 样本2.json libertylite.json
+mv 样本2.json .json
 
 cp 样本.json 样本2.json
 
